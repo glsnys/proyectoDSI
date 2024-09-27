16af6efb6d80df8e5a91110978ae26194fb2f3cd
AldaRodriCh <chalcoaldair26@gmail.com>
2024-09-27 09:53:33 -0500

    CAMBIO

--- index.php
+++ index.php
@@ -12,6 +12,7 @@
         <header class="bg-info">
             Cabecera
             Aldair
+            PEPITO
         </header>
         <nav class="navbar navbar-expand-lg bg-body-tertiary">
             <div class="container-fluid">
